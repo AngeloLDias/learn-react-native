08333a6d1743d54fce58cca5452168f5be74ddd8
Angelo Lima Dias <angelolimadias@gmail.com>
2018-12-26 17:29:42 -0200

    create component and props

--- OneReactNative/App.js
+++ OneReactNative/App.js
@@ -8,6 +8,18 @@ import {
   Image
  } from 'react-native';
 
+
+ class Imagem extends Component{
+   render(){
+     let imagem={
+      uri:"https://abrilexame.files.wordpress.com/2018/04/gm-"+this.props.nome+".jpg"
+    }
+     return(
+        <Image source={imagem} style={{width:parseInt(this.props.largura), height:parseInt(this.props.altura)}} />
+    )
+   }
+ }
+
 export default class PrimeiroProjeto extends Component{
 
   render(){
@@ -19,7 +31,10 @@ export default class PrimeiroProjeto extends Component{
         <Text>Olá Mundo</Text>
         <Text>Olá Mundo</Text>
         <Text>Olá Mundo</Text>
-        <Image source={{uri:"https://abrilexame.files.wordpress.com/2018/04/gm-tracker.jpg"}} style={{width:300,height:300}}/>
+        {/* <Image source={{uri:"https://abrilexame.files.wordpress.com/2018/04/gm-tracker.jpg"}} style={{width:300,height:300}}/> */}
+
+        <Imagem nome="tracker" altura="250" largura="250"/>
+
         <Button title="Aperte" onPress={()=>{
 
           alert('me apertou')
