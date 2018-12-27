3f134e885fb686005268f6b0d43b82677c39a2e7
Angelo Lima Dias <angelolimadias@gmail.com>
2018-12-27 11:11:17 -0200

    flex

--- OneReactNative/App.js
+++ OneReactNative/App.js
@@ -12,28 +12,13 @@ export default class PrimeiroProjeto extends Component{
 
   render(){
       return(
-        <View>
-          <Text style={[styles.azulGrande, styles.textCenter]}>Texto 1</Text>
-          <Text style={styles.textCenter}>Texto 2</Text>
-          <Text>Texto 3</Text>
-          <Text>Texto 4</Text>
+        <View style={{flex:1, backgroundColor:'black'}}>
+            <View style={{flex:1, backgroundColor:'green'}}></View>
+            <View style={{flex:1, backgroundColor:'red'}}></View>
+            <View style={{flex:1, backgroundColor:'yellow'}}></View>
+            <View style={{flex:3, backgroundColor:'blue'}}></View>
         </View>
       );
   } 
   
 }
-
-const styles = StyleSheet.create({
-  texto1:{
-    fontSize:30,
-  },
-  azulGrande:{
-    color:'blue',
-    fontSize:50
-  },
-  textCenter:{
-    textAlign:'center',
-    alignItems:'center',
-    justifyContent:'center',
-  }
-});
