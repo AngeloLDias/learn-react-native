349bdc8b7626ca6a5a388989b4bff1b7ad8988f9
Angelo Lima Dias <angelolimadias@gmail.com>
2018-12-27 10:34:38 -0200

    style groups

--- OneReactNative/App.js
+++ OneReactNative/App.js
@@ -8,52 +8,15 @@ import {
   Image
  } from 'react-native';
 
- class Janta extends Component{
-   
-  constructor(props){
-    super(props);
-    this.state = {comida:props.comida}
-
-    let comida = ['pizza', 'lasanha', 'Hamburguer', 'arroz']
-
-    setInterval(() =>{
-      this.setState(previousState =>{
-        var n = Math.floor( Math.random() * comida.length)
-
-        return {comidas: comida[n]};
-      })
-    }, 1000)
-   }
-   render(){
-     return(
-        <View>
-          <Text style={{
-            textAlign:"center",
-            height:100,
-            // width:100,
-            justifyContent:"center",
-            fontSize:50,
-            alignItems:'center'
-        }}>Hoje a Janta é</Text>
-          <Text style={{
-            textAlign:"center",
-            color:'red',
-            justifyContent:"center",
-            fontSize:50,
-            alignItems:'center'
-        }}>{this.state.comidas}</Text>
-        </View>
-     )
-   }
- }
 export default class PrimeiroProjeto extends Component{
 
   render(){
       return(
         <View>
-        {/* <Text>Olá Mundo</Text> */}
-
-        <Janta comida='Biscoitodfdas' />
+          <Text style={[styles.azulGrande, styles.textCenter]}>Texto 1</Text>
+          <Text style={styles.textCenter}>Texto 2</Text>
+          <Text>Texto 3</Text>
+          <Text>Texto 4</Text>
         </View>
       );
   } 
@@ -61,8 +24,16 @@ export default class PrimeiroProjeto extends Component{
 }
 
 const styles = StyleSheet.create({
-  texto:{
+  texto1:{
     fontSize:30,
-    // color:red
+  },
+  azulGrande:{
+    color:'blue',
+    fontSize:50
+  },
+  textCenter:{
+    textAlign:'center',
+    alignItems:'center',
+    justifyContent:'center',
   }
 });
